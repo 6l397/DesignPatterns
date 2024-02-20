c878984942f00899589424c61be4fa498dbb143e
6l397 <aminasidorets0204@gmail.com>
2024-02-20 09:58:30 +0200

    1lab+ (5 creational design patterns)

--- src/creational/abstractFactory/SimpleVeterinaryFactory.java
+++ src/creational/abstractFactory/SimpleVeterinaryFactory.java
@@ -1,12 +1,12 @@
 package creational.abstractFactory;
 
 import creational.factory.Animal;
-import creational.factory.Dog;
+import creational.factory.Cat;
 
 public class SimpleVeterinaryFactory implements VeterinaryFactory {
     @Override
     public Animal createAnimal() {
-        return new Dog();
+        return new Cat();
     }
 
     @Override
